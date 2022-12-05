a9e0752fb9e99cbc068cad5f0959edfcbe32b0ea
komenAnna <komenriana@gmail.com>
2022-12-05 23:33:47 +0300

    Added first files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello, world!</h1>;
+  }
+}
+
+export default App;
+
+// This code defines a new React component called 'App' that renders a <h1> element
+// It then exports the App component so that it can be imported and used in othet parts of the application
